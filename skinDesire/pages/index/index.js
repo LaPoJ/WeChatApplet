e1041481dd2140040488fd7dca0b35cfb9fe4a13
LP_ZX <2295140520@qq.com>
2019-05-07 15:18:41 +0800

    肤求

---
+++ skinDesire/pages/index/index.js
@@ -0,0 +1,62 @@
+//index.js
+// pages/community/community.js
+
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    // 点赞
+    // thumb: "icon-thumb-up",
+    // // iconThumbDown: "icon-thumb-down",
+    // // 喜欢
+    // heart: "icon-heart-o",
+    // chat: "icon-chat",
+    // chatOne: "icon-chat-one",
+    // chatTwo:"icon-chat-two",
+    // chatMore: "icon-chat-more",
+    // comment: "icon-comment",
+    // share: "icon-share",
+    // star: "icon-star-o"
+  },
+  // ThumbUp:function() {
+  //   var thumb = this.data.thumb
+  //   if (thumb === "icon-thumb-up") {
+  //     this.setData({
+  //       thumb: "icon-thumb-up-fill"
+  //     })
+  //   }
+  //   if (thumb === "icon-thumb-up-fill") {
+  //     this.setData({
+  //       thumb: "icon-thumb-up"
+  //     })
+  //   }
+  // },
+  // ThumbDown: function () {
+  //   var iconThumbDown = this.data.iconThumbDown
+  //   if (iconThumbDown === "icon-thumb-down") {
+  //     this.setData({
+  //       iconThumbDown: "icon-thumb-down-fill"
+  //     })
+  //   }
+  //   if (iconThumbDown === "icon-thumb-down-fill") {
+  //     this.setData({
+  //       iconThumbDown: "icon-thumb-down"
+  //     })
+  //   }
+  // },
+  // toLove: function(){
+  //   var heart = this.data.heart
+  //   if (heart === "icon-heart-o") {
+  //     this.setData({
+  //       heart: "icon-heart-fill"
+  //     })
+  //   }
+  //   if (heart === "icon-heart-fill") {
+  //     this.setData({
+  //       heart: "icon-heart-o"
+  //     })
+  //   }
+  // }
+})
