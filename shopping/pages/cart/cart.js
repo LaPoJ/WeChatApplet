adb32af49682d1483eb766aa603b7ba92b6c7bc7
LP_ZX <2295140520@qq.com>
2019-05-12 10:15:28 +0800

    page 购物车

---
+++ shopping/pages/cart/cart.js
@@ -0,0 +1,167 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    cats: [],
+    hasList: false,
+    totalPrice: 0,
+    selectAllStatus: true,
+  },
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    setTimeout(() => {
+      this.setData({
+        hasList: true,
+        carts: [
+          { id: 1, title: '新鲜芹菜 半斤', image: '/image/s5.png', num: 4, price: 0.01, selected: true },
+          { id: 2, title: '素米 500g', image: '/image/s6.png', num: 1, price: 0.03, selected: true }
+        ]
+      })
+      this.getTotalPrice()
+    }, 1000)
+  },
+  getTotalPrice() {
+    let carts = this.data.carts
+    let total = 0
+    for (let i = 0; i < carts.length; i++) {
+      if (carts[i].selected) {
+        total += carts[i].num * carts[i].price
+      }
+    }
+    this.setData({
+      // carts: carts,
+      totalPrice: total.toFixed(2)
+    })
+  },
+  addCount(e) {
+    const index = e.currentTarget.dataset.index
+    let carts = this.data.carts
+    let num = carts[index].num
+    num = num + 1
+    carts[index].num = num
+    this.setData({
+      carts: carts
+    })
+    this.getTotalPrice()
+  },
+  minusCount(e) {
+    const index = e.currentTarget.dataset.index
+    let carts = this.data.carts
+    let num = carts[index].num
+    if (num <= 1) {
+      wx.showToast({
+        title: '不能再少了，亲',
+        icon: 'none',
+        duration: 1000,
+        mask: true,
+        success: (result)=>{
+
+        },
+        fail: ()=>{},
+        complete: ()=>{}
+      });
+      return false
+    }
+    num = num - 1
+    carts[index].num = num
+    this.setData({
+      carts: carts
+    })
+    this.getTotalPrice()
+  },
+  selectList(e){
+    const index = e.currentTarget.dataset.index
+
+    let carts = this.data.carts
+    if (carts[index].selected) {
+      carts[index].selected = false
+    } else if (carts[index].selected === false){
+      carts[index].selected = true
+    }
+    // console.log(cart);
+    this.setData({
+      carts: carts
+    })
+    this.getTotalPrice()
+  },
+  deleteList(e) {
+    const index = e.currentTarget.dataset.index
+    let carts = this.data.carts
+    carts.splice(index, 1)
+    this.setData({
+      carts: carts
+    })
+    if (!carts.length) {
+      this.setData({
+        hasList: false
+      })
+    } else {
+      this.getTotalPrice()
+    }
+  },
+  selectAll(e) {
+    let selectAllStatus = this.data.selectAllStatus
+    selectAllStatus = !selectAllStatus
+    let carts = this.data.carts
+    for (let i = 0; i < carts.length; i++) {
+      carts[i].selected = selectAllStatus
+    }
+    this.setData({
+      selectAllStatus: selectAllStatus,
+      carts: carts
+    })
+    this.getTotalPrice()
+  },
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  }
+})
