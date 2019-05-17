33e2fbfaa856725d437709b5e300fd30b26b148f
LP_ZX <2295140520@qq.com>
2019-05-17 14:28:19 +0800

    井字棋

---
+++ jzq/game.js
@@ -0,0 +1,14 @@
+import './js/libs/weapp-adapter'
+import './js/libs/symbol'
+
+// 主程序
+  // THREE.js 3D
+  //  Phaser
+  //CoCos2D
+  //Egret -> 国内游戏引擎
+
+//ES6 import + exports
+import Main from './js/main'
+
+
+new Main()
